8ce46c7562e21905f8a35f28732abd328cc0fc7d
vPilug <pilug.vero@gmail.com>
2023-03-21 17:18:36 +0200

    object oriented

---
+++ src/Account.java
@@ -0,0 +1,80 @@
+public class Account {
+    private String number;
+    private double balance;
+    private String customerName;
+    private String customerEmail;
+    private String customerPhone;
+
+    public Account() {
+        this("56789" , 2.50, "Default name",
+                "Default address", "Default called");
+        System.out.println("Empty constructor called");
+    }
+
+    public Account (String number, double balance, String customerName, String email, String phone) {
+        System.out.println("Account contructor with parameters called");
+        this.number = number;
+        this.balance = balance;
+        this.customerName = customerName;
+        customerEmail = email;
+        customerPhone = phone;
+    }
+
+    public Account(String customerName, String customerEmail, String customerPhone) {
+        this("99999", 100.55, customerName,customerEmail, customerPhone);
+    }
+
+    public void depositFunds(double depositAmount) {
+        balance += depositAmount;
+        System.out.println("Deposit of $" + depositAmount + " made. New balance is $" +
+                balance);
+    }
+    public void withdrawFunds(double withdrawalAmount) {
+        if (balance - withdrawalAmount < 0) {
+            System.out.println("Insufficinet Funds! You only have $" + balance +
+                    " in your account. ");
+        } else {
+            balance -= withdrawalAmount;
+            System.out.println("Withdrawal of $" + withdrawalAmount +
+                    " processed, Remaining balance = $" + balance);
+        }
+    }
+
+   public String getNumber() {
+       return number;
+   }
+   public double getBalance() {
+       return balance;
+   }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+    public void setNumber(String number) {
+       this.number = number;
+    }
+    public void setBalance(double balance) {
+       this.balance = balance;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+}
